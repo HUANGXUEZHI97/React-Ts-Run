6d8596152c47e524116fbdaa237df9066f48966b
huangxuezhi <huangxuezhi@wakedata.com>
2021-12-13 11:54:10 +0800

    feat：eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { FC } from 'react';
 import './App.scss';
-import TicTacToe from './components/TicTacToe/index'
+import TicTacToe from './components/TicTacToe/index';
+import HOC from './components/HOC/index';
+import SHooks from './components/UseHooks/state';
 
-const App: FC = () => {
-
+const App: FC = function () {
   return (
     <div>
       {/* 井字棋 */}
       <TicTacToe />
+
+      {/* HOC */}
+      <HOC />
+
+      {/* Hooks */}
+      <SHooks />
     </div>
-  )
-}
+  );
+};
 
 export default App;
